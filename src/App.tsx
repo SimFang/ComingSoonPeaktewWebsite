3d1321bc53b1945360e7402826001618be3f8dbf
AuraSolution <aurasolutionagency@outlook.com>
2025-04-25 20:14:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import "./App.css";
+import linkedinIcon from "./assets/linkedin.jpg";
+import instagramIcon from "./assets/instagram.jpg";
+import tiktokIcon from "./assets/tiktok.jpg";
+import peakTewLogo from "./assets/logo.jpg";
+import peopleImage from "./assets/background.jpg";
+import mascotImage from "./assets/mascot.png";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="social-icons">
+        <a
+          href="https://www.linkedin.com/company/peaktew/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedinIcon} alt="LinkedIn" className="social-icon" />
+        </a>
+        <a
+          href="https://www.instagram.com/peaktew/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" className="social-icon" />
+        </a>
+        <a
+          href="https://www.tiktok.com/@peaktew"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={tiktokIcon} alt="Ticktok" className="social-icon" />
+        </a>
+      </div>
+
+      <div className="logo">
+        <img src={peakTewLogo} alt="PeakTew Logo" />
+      </div>
+
+      <h1 className="coming-soon-text">Coming Soon...</h1>
+
+      <div className="full-width-background">
+        <img src={peopleImage} alt="background" className="full-bg-image" />
+        <img src={mascotImage} alt="Purple mascot" className="mascot-image" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
